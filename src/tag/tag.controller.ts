0f72b7451d4afc9e48a79befee6b3ebc35dc971e
Dmytro <ihnatenko.dev@gmail.com>
2023-05-06 18:23:58 +0300

    Return tags data: { tags: Array<string> }

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TagService } from './tag.service';
-import { TagEntity } from './tag.entity';
 
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Get()
-  async findAll(): Promise<Array<TagEntity>> {
-    return await this.tagService.findAll();
+  async findAll(): Promise<{ tags: Array<string> }> {
+    const tags = await this.tagService.findAll();
+    return { tags: tags.map((tag) => tag.name) };
   }
 }
