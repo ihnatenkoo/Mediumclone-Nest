f6b124a1ad6d7cb923b4846818bb66bfc86ee558
Dmytro <ihnatenko.dev@gmail.com>
2023-05-07 16:49:19 +0300

    Impl hash pass in user entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { hash } from 'bcrypt';
 
 @Entity({ name: 'users' })
 export class UserEntity {
@@ -19,4 +20,9 @@ export class UserEntity {
 
   @Column({ default: '' })
   image: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await hash(this.password, 10);
+  }
 }
