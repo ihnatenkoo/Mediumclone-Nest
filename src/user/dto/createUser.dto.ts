b0d36a2b1ac1820e7878db732547280b556355ba
Dmytro <ihnatenko.dev@gmail.com>
2023-05-09 11:49:19 +0300

    Fix create user dto

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,13 +1,22 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty()
+  @MinLength(3, {
+    message: 'Username must contain more than 2 symbols',
+  })
+  @MaxLength(9, {
+    message: 'Username must contain less than 10 symbols',
+  })
   readonly username: string;
 
-  @IsNotEmpty()
   @IsEmail()
   readonly email: string;
 
-  @IsNotEmpty()
+  @MinLength(5, {
+    message: 'Password must contain more than 4 symbols',
+  })
+  @MaxLength(14, {
+    message: 'Password must contain less than 15 symbols',
+  })
   readonly password: string;
 }
