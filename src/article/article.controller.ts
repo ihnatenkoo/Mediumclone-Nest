77cee5c2af3481d02453b961735ff6d0027d41b7
Dmytro <ihnatenko.dev@gmail.com>
2023-05-15 17:23:35 +0300

    Create [post] api/articles createArticle method

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/user/guards/auth.guard';
+import { User } from 'src/user/decorators/user.decorator';
+import { UserEntity } from 'src/user/user.entity';
+import { CreateArticleDto } from './dto/createArticle.dto';
 import { ArticleService } from './article.service';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createArticle(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<any> {
+    return this.articleService.createArticle(currentUser, createArticleDto);
+  }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from 'src/user/user.entity';
+import { ArticleEntity } from './article.entity';
+import { CreateArticleDto } from './dto/createArticle.dto';
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.author = currentUser;
+
+    //TODO: generate slug
+    article.slug = 'foo';
+
+    return await this.articleRepository.save(article);
+  }
+}
