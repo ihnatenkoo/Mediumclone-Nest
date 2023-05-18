971d5b3b6f5696096164334008a013656f733440
Dmytro <ihnatenko.dev@gmail.com>
2023-05-18 16:41:43 +0300

    Implement getProfile [get] api/profiles/:username

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ProfileService } from './profile.service';
+import { IProfileResponse } from './types/profileResponse.interface';
 
 @Controller('profiles')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @Get(':username')
-  getProfile(@Param('username') username: string) {
-    return this.profileService.getProfile(username);
+  async getProfile(
+    @Param('username') username: string,
+  ): Promise<IProfileResponse> {
+    const profile = await this.profileService.getProfile(username);
+
+    return this.profileService.buildProfileResponse(profile);
   }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
+import { IProfileResponse } from './types/profileResponse.interface';
+import { ProfileType } from './types/profile.type';
 
 @Injectable()
 export class ProfileService {
-  getProfile(username: string) {
-    return username;
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async getProfile(username: string): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    return { ...user, following: false };
+  }
+
+  buildProfileResponse(profile: ProfileType): IProfileResponse {
+    const { username, bio, image, following } = profile;
+
+    return { profile: { username, bio, image, following } };
   }
 }
